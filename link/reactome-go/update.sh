034cb7c3cd473b891c1b86c46d7b2a139de922e5
moriya-dbcls <moriya@enoki.local>
2021-02-04 11:49:41 +0900

    update shell script

--- link/reactome-go/update.sh
+++ link/reactome-go/update.sh
@@ -1,19 +1,19 @@
 # 20210204 moriya
 
 # SPARQL query
-QUERY="PREFIX biopax: <http://www.biopax.org/release/biopax-level3.owl#> \
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
-SELECT DISTINCT ?reactome ?go \
-FROM <http://rdf.ebi.ac.uk/dataset/reactome> \
-WHERE { \
-  ?path biopax:xref [ \
-    biopax:db 'Reactome'^^xsd:string ; \
-    biopax:id ?reactome \
-  ] ; \
-        biopax:xref [ \
-    biopax:db 'GENE ONTOLOGY'^^xsd:string ; \
-    biopax:id ?go \
-  ] . \
+QUERY="PREFIX biopax: <http://www.biopax.org/release/biopax-level3.owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+SELECT DISTINCT ?reactome ?go
+FROM <http://rdf.ebi.ac.uk/dataset/reactome>
+WHERE {
+  ?path biopax:xref [
+    biopax:db 'Reactome'^^xsd:string ;
+    biopax:id ?reactome
+  ] ;
+  biopax:xref [
+    biopax:db 'GENE ONTOLOGY'^^xsd:string ;
+    biopax:id ?go
+  ] .
 }"
 
 # url encode
