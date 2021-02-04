18fad27822c0fc1237cbeb9d62b59e27eae931d0
moriya-dbcls <moriya@enoki.local>
2021-02-04 12:49:26 +0900

    update script

---
+++ link/wikipathways-doid/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# 20210204 moriya
+
+# SPARQL query
+QUERY="PREFIX wp: <http://vocabularies.wikipathways.org/wp#>
+SELECT DISTINCT ?pathway ?doid
+WHERE {
+  ?pathway a wp:Pathway ;
+           wp:diseaseOntologyTag ?doid .
+}"
+
+# curl -> format -> delete header
+curl -H "Accept: text/csv" --data-urlencode "query=$QUERY" http://sparql.wikipathways.org/sparql | sed -e 's/\"//g; s/http:\/\/identifiers\.org\/wikipathways\///g; s/http:\/\/purl\.obolibrary\.org\/obo\///g; s/,/\t/g' | sed -n 2,\$p
+
