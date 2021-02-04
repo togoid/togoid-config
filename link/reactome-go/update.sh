1d1a39ba2701c720a0baf645759adb3efcb05f01
moriya-dbcls <moriya@enoki.local>
2021-02-04 12:53:41 +0900

    update script

--- link/reactome-go/update.sh
+++ link/reactome-go/update.sh
@@ -18,9 +18,6 @@ WHERE {
   ] .
 }"
 
-# url encode
-ENCODE=`echo $QUERY | nkf -WwMQ | sed 's/=$//g' | tr = % | tr -d '\n'`
-
-# curl post -> format -> delete header
-curl -X POST -H "Accept: text/csv" -d "query=$ENCODE" https://integbio.jp/rdf/mirror/ebi/sparql | sed -e 's/\"//g; s/,/\t/g; s/GO:/GO_/g' | sed -n 2,\$p
+# curl -> format -> delete header
+curl -H "Accept: text/csv" --data-urlencode "query=$QUERY" https://integbio.jp/rdf/mirror/ebi/sparql | sed -e 's/\"//g; s/,/\t/g; s/GO:/GO_/g' | sed -n 2,\$p
 
