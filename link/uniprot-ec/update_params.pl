93626f3effadf9585777a6573cc9915984c63b75
moriya-dbcls <moriya@enoki.local>
2021-02-04 15:55:42 +0900

    add uniprot-ec

---
+++ link/uniprot-ec/update_params.pl
@@ -0,0 +1,33 @@
+# Thread limit of SPARQL query
+our $THREAD_LIMIT = 10;
+
+# Endpoint
+our $EP = "https://integbio.jp/rdf/mirror/uniprot/sparql";
+our $EP_MIRROR = "https://sparql.uniprot.org/sparql";
+
+# SPARQL query for get-taxonomy-list
+our $QUERY_TAX = "PREFIX up: <http://purl.uniprot.org/core/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX db: <http://purl.uniprot.org/database/>
+SELECT DISTINCT ?org
+WHERE {
+  ?org ^up:organism [ 
+    a up:Protein ;
+    up:enzyme ?ec
+  ] .
+}";
+
+# SPARQL query for get-ID-list
+our $QUERY = "PREFIX up: <http://purl.uniprot.org/core/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX db: <http://purl.uniprot.org/database/>
+SELECT DISTINCT ?source ?target
+WHERE {
+  ?source a up:Protein ;
+          up:organism <__TAXON__> ;
+          up:enzyme ?target .
+}";
+
+# regex : req. double escape backslash (e.g. '\d' -> '\\d')
+our $SOURCE_REGEX = "http://purl.uniprot.org/uniprot/(.+)";
+our $TARGET_REGEX = "http://purl.uniprot.org/enzyme/(.+)";
