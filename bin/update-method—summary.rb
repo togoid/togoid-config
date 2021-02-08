55a210494c590356697bb93579f802aa31b4e3ef
hono <hono@kurofune.local>
2021-02-08 14:44:52 +0900

    Add update-method—summary.rb

---
+++ bin/update-method—summary.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+# Usage: freq-summary.rb link/*/*.yaml
+#
+
+require 'yaml'
+
+ARGV.each do |yaml|
+  begin
+    config = YAML.load(File.read(yaml))
+    pair = File.basename(File.dirname(yaml))
+    puts [pair, config["update"]["method"]].join("\t")
+  rescue => error
+    puts [yaml, error].join("\t")
+    next
+  end
+end
+
+
+
