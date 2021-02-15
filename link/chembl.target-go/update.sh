ae7a9a3d8377253102b6cfdb350f30dd155f8e62
yatsuzuka-nbdc <33243937+yatsuzuka-nbdc@users.noreply.github.com>
2021-02-15 16:24:02 +0900

    Update update.sh

--- link/chembl.target-go/update.sh
+++ link/chembl.target-go/update.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # SPARQL query
 QUERY="PREFIX cco: <http://rdf.ebi.ac.uk/terms/chembl#>
-SELECT ?ChemblTargetcomponent ?go
+SELECT ?chembltarget ?go
 WHERE{
   ?ChemblTargetcomponent a cco:TargetComponent .
-  ?ChemblTargetcomponent cco:targetCmptXref ?go .
-  FILTER regex (?go, "GO", "i") .
+  ?ChemblTargetcomponent cco:targetCmptXref ?go_uri .
+  FILTER regex (?go_uri, "GO", "i") .
+  
+  BIND (REPLACE(STR(?ChemblTargetcomponent), 'http://rdf.ebi.ac.uk/resource/chembl/targetcomponent/', '') AS ?chembltarget)
+  BIND (REPLACE(STR(?go_uri), 'http://identifiers.org/obo.go/', '') AS ?go)
   }
-  ORDER BY ?ChemblTargetcomponent"
+  ORDER BY ?chembltarget"
 # curl -> format -> delete header
 curl -s -H "Accept: text/csv" --data-urlencode "query=$QUERY" https://integbio.jp/rdf/mirror/ebi/sparql | sed -e 's/\"//g;  s/,/\t/g' | sed -e '1d' > pair.tsv
