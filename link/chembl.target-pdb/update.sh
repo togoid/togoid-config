eeb295685cb2d5bef97949bfb79399b6ce689bd5
yatsuzuka-nbdc <33243937+yatsuzuka-nbdc@users.noreply.github.com>
2021-02-16 14:39:51 +0900

    Create update.sh

---
+++ link/chembl.target-pdb/update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# SPARQL query
+QUERY="PREFIX cco: <http://rdf.ebi.ac.uk/terms/chembl#>
+SELECT ?moleculeid ?pdb_id
+WHERE{
+  ?mechanism a cco:Mechanism ;
+     cco:hasTarget ?target ;
+     cco:hasMolecule ?molecule .
+  ?target cco:hasTargetComponent ?component .
+  ?component cco:targetCmptXref ?pdb .
+  FILTER(CONTAINS(STR(?pdb), "pdb"))
+  BIND (REPLACE(STR(?pdb), 'http://identifiers.org/pdb/', '') AS ?pdb_id)
+  ?molecule cco:chemblId ?moleculeid .     
+  }
+ORDER BY ?moleculeid"
+# curl -> format -> delete header
+curl -s -H "Accept: text/csv" --data-urlencode "query=$QUERY" https://integbio.jp/rdf/mirror/ebi/sparql | sed -e 's/\"//g;  s/,/\t/g' | sed -e '1d' > pair.tsv
