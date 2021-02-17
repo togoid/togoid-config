5686cb84f0638c99e1b0e514f3e06cc14eeed52b
yatsuzuka-nbdc <33243937+yatsuzuka-nbdc@users.noreply.github.com>
2021-02-17 10:48:54 +0900

    Add files via upload

--- link/hint-uniprot/update.sh
+++ link/hint-uniprot/update.sh
@@ -4,7 +4,6 @@ QUERY="PREFIX hnt: <http://purl.jp/10/hint/>
 PREFIX bp3: <http://www.biopax.org/release/biopax-level3.owl#>
 PREFIX uni: <http://identifiers.org/uniprot/>
 PREFIX obo: <http://purl.obolibrary.org/obo/>
-
 SELECT  (replace(str(?hint), "http://purl.jp/10/hint/", "") as ?hint_id)   (replace(str(?uniprot), "http://identifiers.org/uniprot/", "") as ?uniprot_id)
 FROM <http://med2rdf.org/graph/hint> 
 where {
@@ -13,4 +12,4 @@ where {
   }
 ORDER BY ?hint_id"
 # curl -> format -> delete header
-curl -s -H "Accept: text/csv" --data-urlencode "query=$QUERY" http://sparql.med2rdf.org/sparql | sed -e 's/\"//g;  s/,/\t/g' | sed -e '1d' > pair.tsv
+curl -s -H "Accept: text/csv" --data-urlencode "query=$QUERY" http://sparql.med2rdf.org/sparql | sed -e 's/\"//g;  s/,/\t/g' | sed -e '1d' > link.tsv
