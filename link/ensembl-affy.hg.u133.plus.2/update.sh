2569b9a5f5a00ec18472d6501fc13955875f89aa
Hiromasa Ono <hono@dbcls.rois.ac.jp>
2021-02-17 20:16:28 +0900

    Change wget option to send results to stdout

--- link/ensembl-affy.hg.u133.plus.2/update.sh
+++ link/ensembl-affy.hg.u133.plus.2/update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # 20210216 hiromasaono
 # wget
-wget -q -O pair.tsv 'http://www.ensembl.org/biomart/martservice?query=<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE Query><Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "1" count = "" datasetConfigVersion = "0.6" ><Dataset name = "hsapiens_gene_ensembl" interface = "default" ><Attribute name = "ensembl_gene_id" /><Attribute name = "affy_hg_u133_plus_2" /></Dataset></Query>'
+wget -q -O - 'http://www.ensembl.org/biomart/martservice?query=<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE Query><Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "1" count = "" datasetConfigVersion = "0.6" ><Dataset name = "hsapiens_gene_ensembl" interface = "default" ><Attribute name = "ensembl_gene_id" /><Attribute name = "affy_hg_u133_plus_2" /></Dataset></Query>'
