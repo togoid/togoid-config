16697ba943b14fbba496ebe9568721e6185e39ee
ShinJM <48198388+ShinJM@users.noreply.github.com>
2021-02-17 18:42:27 +0900

    created dgidb

---
+++ link/dgidb-chembl/dgidb-chembl.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# 20210216 shin
+# SPARQL query
+QUERY="PREFIX dgio: <http://purl.jp/bio/10/dgidb/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX dcterm: <http://purl.org/dc/terms/>
+
+SELECT ?DGIDdb_id ?dgidb_drug_id
+WHERE {
+  ?DGIdbInteraction a dgio:Interaction ;
+                    dcterm:identifier ?DGIDdb_id ;
+                    dgio:drug ?DGIdbDrug .
+  ?DGIdbDrug a dgio:Drug ;
+             rdfs:seeAlso ?dgidb_drug .
+  BIND (replace(str(?dgidb_drug), 'http://rdf.ebi.ac.uk/resource/chembl/molecule/', '') AS ?dgidb_drug_id)
+}"
+# curl -> format -> delete header
+curl -H "Accept: text/csv" --data-urlencode "query=$QUERY" http://sparql.med2rdf.org/sparql | sed -e 's/\"//g; s/,/\t/g' | sed -e '1d'> pair.tsv
