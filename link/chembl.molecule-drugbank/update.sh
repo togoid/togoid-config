6f54dad3516c7ba67386bc3e69cc7c67050f4d37
yatsuzuka-nbdc <33243937+yatsuzuka-nbdc@users.noreply.github.com>
2021-02-17 10:54:00 +0900

    Create configs (Tomoe Nobusada)

---
+++ link/chembl.molecule-drugbank/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# SPARQL query
+QUERY="PREFIX cco: <http://rdf.ebi.ac.uk/terms/chembl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT (replace(str(?molecule), "http://rdf.ebi.ac.uk/resource/chembl/molecule/", "") as ?chembl_id) (replace(str(?drugbank), "http://www.drugbank.ca/drugs/", "") as ?drugbank_id) 
+WHERE{
+  ?molecule  a cco:SmallMolecule ;
+      cco:moleculeXref ?drugbank .
+  FILTER(CONTAINS(STR(?drugbank), "drugbank"))
+  }
+ORDER BY ?molecule"
+# curl -> format -> delete header
+curl -s -H "Accept: text/csv" --data-urlencode "query=$QUERY" https://integbio.jp/rdf/mirror/ebi/sparql | sed -e 's/\"//g;  s/,/\t/g' | sed -e '1d' > link.tsv
