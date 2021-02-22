bd0ed28c79db3365ac30a5b771a3b4fc32b7356b
Toshiaki Katayama <k@bioruby.org>
2021-02-22 18:06:23 +0900

    Removed DOID: and OMIM: prefix

--- link/doid-omim/doid-omim.sh
+++ link/doid-omim/doid-omim.sh
@@ -8,8 +8,8 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema>
 SELECT DISTINCT ((?s)AS ?ORDO_ID) (STR(?id) AS ?ID)
 FROM <http://integbio.jp/rdf/mirror/bioportal/doid>
 WHERE {
-  ?s oboInOwl:hasDbXref ?id.
-   FILTER(contains(?id,'OMIM'))
-        }"
+  ?s oboInOwl:hasDbXref ?id .
+  FILTER(contains(?id,'OMIM'))
+}"
 # curl -> format -> delete header
-curl -H "Accept: text/csv" --data-urlencode "query=$QUERY" https://integbio.jp/rdf/mirror/bioportal/sparql | sed -e 's/\"//g; s/,/\t/g; s/http:\/\/purl.obolibrary.org\/obo\/DOID_/DOID:/g' | sed -e '1d'> pair.tsv
+curl -s -H "Accept: text/csv" --data-urlencode "query=$QUERY" https://integbio.jp/rdf/mirror/bioportal/sparql | sed -e 's/\"//g; s/,/\t/g; s/http:\/\/purl.obolibrary.org\/obo\/DOID_//g; s/OMIM://' | sed -e '1d'
