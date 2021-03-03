2a4fb46770eeb2e48c94caa5f59cdb53151ea326
Toshiaki Katayama <k@bioruby.org>
2021-03-03 11:54:24 +0900

    Updated config.yaml (introduced catalog and category, dropped namespace from source and target)

--- lib/togoid-config.rb
+++ lib/togoid-config.rb
@@ -1,20 +1,22 @@
+require 'fileutils'
+
 module TogoID
 
   class Node
-    attr_reader :ns, :dt, :label, :prefix
+    attr_reader :catalog, :category, :label, :prefix
     def initialize(hash)
-      @ns = hash["namespace"]
-      @dt = hash["type"]
+      @catalog = hash["catalog"]
+      @category = hash["category"]
       @label = hash["label"]
       @prefix = hash["prefix"]
     end
   end
 
   class Edge
-    attr_reader :ns, :label, :prefix, :predicate
+    attr_reader :label, :ns, :prefix, :predicate
     def initialize(hash)
-      @ns = hash["namespace"]
       @label = hash["label"]
+      @ns = hash["namespace"]
       @prefix = hash["prefix"]
       @predicate = hash["predicate"]
     end
@@ -35,13 +37,6 @@ module TogoID
       @frequency = hash["frequency"]
       @method = hash["method"]
     end
-
-    def run(path)
-      Dir.chdir(path) do
-        ENV['PATH'] = ".:#{ENV['PATH']}"
-        system(@method)
-      end
-    end
   end
 
   class Config
@@ -50,6 +45,7 @@ module TogoID
       begin
         config = YAML.load(File.read(config_file))
         @path = File.dirname(config_file)
+        @source_ns, @target_ns = File.basename(@path).split('-')
         @source = Node.new(config["source"])
         @target = Node.new(config["target"])
         @link = Link.new(config["link"])
@@ -58,6 +54,16 @@ module TogoID
         puts error
         exit 1
       end
+      setup_files
+    end
+
+    def setup_files
+      @tsv_dir = "output/tsv"
+      @ttl_dir = "output/ttl"
+      @tsv_file = "#{@tsv_dir}/#{@source_ns}-#{@target_ns}.tsv"
+      @ttl_file = "#{@ttl_dir}/#{@source_ns}-#{@target_ns}.ttl"
+      FileUtils.mkdir_p(@tsv_dir)
+      FileUtils.mkdir_p(@ttl_dir)
     end
 
     def triple(s, p, o)
@@ -65,34 +71,57 @@ module TogoID
     end
 
     def prefix
+      prefixes = []
       if @link.fwd
-        puts triple("@prefix", "#{@link.fwd.ns}:", "<#{@link.fwd.prefix}>")
+        prefixes << triple("@prefix", "#{@link.fwd.ns}:", "<#{@link.fwd.prefix}>")
       end
       if @link.rev and (! @link.fwd or (@link.fwd.ns != @link.rev.ns))
-        puts triple("@prefix", "#{@link.rev.ns}:", "<#{@link.rev.prefix}>")
+        prefixes << triple("@prefix", "#{@link.rev.ns}:", "<#{@link.rev.prefix}>")
       end
-      puts triple("@prefix", "#{@source.ns}:", "<#{@source.prefix}>")
-      puts triple("@prefix", "#{@target.ns}:", "<#{@target.prefix}>")
-      puts
+      prefixes << triple("@prefix", "#{@source_ns}:", "<#{@source.prefix}>")
+      prefixes << triple("@prefix", "#{@target_ns}:", "<#{@target.prefix}>")
+      return prefixes
     end
 
     def exec_convert
-      prefix
-      @link.files.each do |file|
-        File.open("#{@path}/#{file}").each do |line|
-          source_id, target_id, = line.strip.split(/\s+/)
-          puts triple("#{@source.ns}:#{source_id}", "#{@link.fwd.ns}:#{@link.fwd.predicate}", "#{@target.ns}:#{target_id}") if @link.fwd
-          puts triple("#{@target.ns}:#{target_id}", "#{@link.rev.ns}:#{@link.rev.predicate}", "#{@source.ns}:#{source_id}") if @link.rev
+      if File.exists?(@tsv_file)
+        File.open(@ttl_file, "w") do |ttl_file|
+          ttl_file.puts prefix
+          ttl_file.puts
+          tsv2ttl(@tsv_file, ttl_file)
         end
+      else
+        $stderr.puts "TogoID TSV file #{@tsv_file} not found. Run update first."
+      end
+    end
+
+    def tsv2ttl(tsv, ttl)
+      File.open(tsv).each do |line|
+        source_id, target_id, = line.strip.split(/\s+/)
+        ttl.puts triple("#{@source_ns}:#{source_id}", "#{@link.fwd.ns}:#{@link.fwd.predicate}", "#{@target_ns}:#{target_id}") if @link.fwd
+        ttl.puts triple("#{@target_ns}:#{target_id}", "#{@link.rev.ns}:#{@link.rev.predicate}", "#{@source_ns}:#{source_id}") if @link.rev
       end
     end
 
     def exec_update
-      @update.run(@path)
+      ENV['PATH'] = [ File.expand_path('bin'), File.expand_path(@path), ENV['PATH'] ].join(':')
+      File.open(@tsv_file, "w") do |tsv_file|
+        Dir.chdir(@path) do
+          IO.popen(@update.method) do |io|
+            tsv_file.puts io.read
+          end
+        end
+      end
     end
 
     def exec_check
-      pp self
+      $stderr.puts pp(self)
+      $stderr.puts
+      puts prefix
+      puts
+      @link.files.each do |file|
+        tsv2ttl("#{@path}/#{file}", $stdout)
+      end
     end
   end
 
