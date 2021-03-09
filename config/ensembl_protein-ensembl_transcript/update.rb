096e0529905451946a9d2b5fb83fa775cc6afca6
Toshiaki Katayama <k@bioruby.org>
2021-03-10 01:10:37 +0900

    Simplified ensembl_protein-ensembl_transcript for refactoring

---
+++ config/ensembl_protein-ensembl_transcript/update.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+query_file    = ARGV.shift || "query.rq"
+endpoint      = ARGV.shift || "https://integbio.jp/rdf/ebi/sparql"
+taxonomy_list = ARGV.shift || "../../input/ensembl/taxonomy.txt"
+
+query_template = File.read(query_file)
+
+File.read(taxonomy_list).split("\n").each do |tax_id|
+  query = query_template.sub('{{tax_id}}', tax_id)
+  cmd = "curl -s -H 'Accept: text/csv' --data-urlencode 'query=#{query}' #{endpoint} | sed -E 1d"
+  IO.popen(cmd) do |io|
+    while buffer = io.gets
+      puts buffer.gsub(',', "\t").gsub('"', '')
+    end
+  end
+end
