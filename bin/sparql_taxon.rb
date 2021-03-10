02b298c8e05dabe5b5da094bdad3fc78823a7bcc
Toshiaki Katayama <k@bioruby.org>
2021-03-11 08:44:23 +0900

    Refactored ensembl* to use sparql_taxon.rb

--- bin/sparql_taxon.rb
+++ bin/sparql_taxon.rb
@@ -6,8 +6,8 @@ taxonomy_list = ARGV.shift || "../../input/ensembl/taxonomy.txt"
 
 query_template = File.read(query_file)
 
-File.read(taxonomy_list).split("\n").each do |tax_id|
-  query = query_template.sub('{{tax_id}}', tax_id)
+File.read(taxonomy_list).split("\n").each do |taxon|
+  query = query_template.sub('{{taxon}}', taxon)
   cmd = "curl -s -H 'Accept: text/csv' --data-urlencode 'query=#{query}' #{endpoint} | sed -E 1d"
   IO.popen(cmd) do |io|
     while buffer = io.gets
