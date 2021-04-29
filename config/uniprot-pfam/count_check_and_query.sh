7ffbf84918187d8e05867f40b68fb92d4303dac1
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-04-29 09:14:24 +0900

    古いシェルスクリプトの削除と軽微な修正。

--- config/uniprot-pfam/count_check_and_query.sh
+++ config/uniprot-pfam/count_check_and_query.sh
@@ -6,19 +6,20 @@ set -euo pipefail
 TARGET=$1
 ENDPOINT=$2
 LIMIT=$3
+CURL=/usr/bin/curl
 #ENDPOINT=https://integbio.jp/rdf/mirror/uniprot/sparql
 PAIR=($(wc -l $TARGET))
 if [ ${PAIR[0]} -gt $LIMIT ] ; then
   MTM=$(echo ${PAIR[1]} | grep -o 'PF[0-9]*')
   QUERY=$(sed -e "s/__PFAM_ID__/${MTM}/" get_uniprot_pfam_list_pfam_count.rq)
 #  echo $QUERY
-  TOTAL=$(curl -s -H "Accept: text/tab-separated-values" --data-urlencode query="$QUERY" $ENDPOINT | tail -n +2)
+  TOTAL=$($CURL -s -H "Accept: text/tab-separated-values" --data-urlencode query="$QUERY" $ENDPOINT | tail -n +2)
   COUNT=$(expr $TOTAL / $LIMIT)
 #  echo $COUNT
-  for i in $(seq ${COUNT});
+  for i in $(seq ${COUNT})
   do
     IQUERY=$(sed -e "s/__PFAM_ID__/${MTM}/" -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" get_uniprot_pfam_list_pfam.rq)
 #    echo $IQUERY
-    curl -sSH "Accept: text/tab-separated-values" --data-urlencode query="$IQUERY" $ENDPOINT | tail -n +2 >> $TARGET
+    $CURL -sSH "Accept: text/tab-separated-values" --data-urlencode query="$IQUERY" $ENDPOINT | tail -n +2 >> $TARGET
   done
 fi
--- config/uniprot-pfam/retrieve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-
-# 2021/04/13時点で186,681,389件(本家で確認)あるので、適宜seqの引数を増やす必要あり。
-
-seq -f '%.0f' 0 1000000 190000000 | xargs -i sh -c "curl -sSH 'Accept: text/tab-separated-values' --data-urlencode query='PREFIX up: <http://purl.uniprot.org/core/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX db: <http://purl.uniprot.org/database/> SELECT (substr(str(?uniprot),33) as ?uniprot_id) (substr(str(?target), 30) as ?target_id) { SELECT DISTINCT ?uniprot ?target ?g1 { GRAPH <http://sparql.uniprot.org/uniprot> { ?uniprot a up:Protein ; rdfs:seeAlso ?target . ?target up:database db:Pfam .}} offset "{}" limit 1000000}' https://integbio.jp/rdf/mirror/uniprot/sparql | tail -n +2" | sed -e 's/"//g'
