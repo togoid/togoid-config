6adf3100aaee3ab40a3d46922e2a23b7604dbe5c
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-04-29 16:22:39 +0900

    エラーチェック機能を追加。

--- config/uniprot-pfam/retrieve_uniprot_pfam.sh
+++ config/uniprot-pfam/retrieve_uniprot_pfam.sh
@@ -6,6 +6,10 @@ WORKDIR=pfam2uniprot # 一時的にPfamID毎のUniProtIDリストファイルを
 LIMIT=1000000 # SPARQLエンドポイントにおける取得可能データ件数の最大値
 CURL=/usr/bin/curl
 
+# Pfam ID単位で対応するUniProt IDを取得するクエリのテンプレート。
+# __PFAM_ID__ と書かれている部分を、実際のPfam ID（ex. PF00201）で置換する。
+QUERY_FILE=get_uniprot_pfam_list_pfam.rq
+
 if [ ! -e $WORKDIR ]; then mkdir $WORKDIR ; fi
 ls ${WORKDIR}/*.txt &> /dev/null
 if [ $? = 0 ]; then rm ${WORKDIR}/*; fi
@@ -14,7 +18,18 @@ if [ $? = 0 ]; then rm ${WORKDIR}/*; fi
 $CURL -sSH 'Accept: text/tab-separated-values' --data-urlencode query@get_pfam_id.rq $ENDPOINT | grep -o 'PF[0-9]*' > pfam_id_list.txt
 
 # Pfam ID毎にUniProt IDを取得。
-cat pfam_id_list.txt | xargs -P20 -i sh -c "QUERY=\$(sed -e 's/__PFAM_ID__/{}/' get_uniprot_pfam_list_pfam.rq); $CURL -o ${WORKDIR}/{}.txt -sSH 'Accept: text/tab-separated-values' --data-urlencode query=\"\${QUERY}\" $ENDPOINT"
+cat pfam_id_list.txt | xargs -P20 -i sh -c "QUERY=\$(sed -e 's/__PFAM_ID__/{}/' $QUERY_FILE); $CURL -o ${WORKDIR}/{}.txt -sSH 'Accept: text/tab-separated-values' --data-urlencode query=\"\${QUERY}\" $ENDPOINT"
+
+# エラー終了しているファイルを検索し、改めて検索、をエラーがなくなるまで行う。
+# ファイル冒頭に "uniprot_id" が書かれていない場合に、エラーと判断する。
+while true; do 
+  ERROR_FILES=$(find ${WORKDIR} -type f -exec sh -c 'grep -q "^\"uniprot_id\"" {} || basename {} .txt' \;)
+  if [ -n "$ERROR_FILES" ]; then
+     echo -n $ERROR_FILES | xargs -P20 -i -d ' ' sh -c "QUERY=\$(sed -e 's/__PFAM_ID__/{}/' $QUERY_FILE); $CURL -o ${WORKDIR}/{}.txt -sSH 'Accept: text/tab-separated-values' --data-urlencode query=\"\${QUERY}\" $ENDPOINT"
+  else
+     break
+  fi
+done
 
 # 100万件以上ヒットしているPfam IDを検索し、残りを漏れなく取得する。
 find ${WORKDIR} -type f -exec sh count_check_and_query.sh "{}" $ENDPOINT $LIMIT \;
