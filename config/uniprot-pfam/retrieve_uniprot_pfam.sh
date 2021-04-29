f99e6b9c12f04f13854238f04067921a9869454b
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-04-29 09:14:24 +0900

    古いシェルスクリプトの削除と軽微な修正。

--- config/uniprot-pfam/retrieve_uniprot_pfam.sh
+++ config/uniprot-pfam/retrieve_uniprot_pfam.sh
@@ -4,16 +4,17 @@ set -euo pipefail
 ENDPOINT=https://integbio.jp/rdf/mirror/uniprot/sparql
 WORKDIR=pfam2uniprot # 一時的にPfamID毎のUniProtIDリストファイルを保存するディレクトリ
 LIMIT=1000000 # SPARQLエンドポイントにおける取得可能データ件数の最大値
+CURL=/usr/bin/curl
 
 if [ ! -e $WORKDIR ]; then mkdir $WORKDIR ; fi
 ls ${WORKDIR}/*.txt &> /dev/null
 if [ $? = 0 ]; then rm ${WORKDIR}/*; fi
 
 # Pfam IDのリストを取得。
-curl -sSH 'Accept: text/tab-separated-values' --data-urlencode query@get_pfam_id.rq $ENDPOINT | grep -o 'PF[0-9]*' > pfam_id_list.txt
+$CURL -sSH 'Accept: text/tab-separated-values' --data-urlencode query@get_pfam_id.rq $ENDPOINT | grep -o 'PF[0-9]*' > pfam_id_list.txt
 
 # Pfam ID毎にUniProt IDを取得。
-cat pfam_id_list.txt | xargs -P20 -i sh -c "QUERY=\$(sed -e 's/__PFAM_ID__/{}/' get_uniprot_pfam_list_pfam.rq); curl -o ${WORKDIR}/{}.txt -sSH 'Accept: text/tab-separated-values' --data-urlencode query=\"\${QUERY}\" $ENDPOINT"
+cat pfam_id_list.txt | xargs -P20 -i sh -c "QUERY=\$(sed -e 's/__PFAM_ID__/{}/' get_uniprot_pfam_list_pfam.rq); $CURL -o ${WORKDIR}/{}.txt -sSH 'Accept: text/tab-separated-values' --data-urlencode query=\"\${QUERY}\" $ENDPOINT"
 
 # 100万件以上ヒットしているPfam IDを検索し、残りを漏れなく取得する。
 find ${WORKDIR} -type f -exec sh count_check_and_query.sh "{}" $ENDPOINT $LIMIT \;
