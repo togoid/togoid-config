434109817f85da6e3cf50b6b72dc91fa766423c3
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-09-07 16:58:31 +0900

    DOIDの出力形式を数字のみになるように修正。

--- config/glytoucan-doid/gen_pairs.pl
+++ config/glytoucan-doid/gen_pairs.pl
@@ -11,14 +11,14 @@ while(<>){
     next;
   }
   my @vals = split /\t/;
-  $vals[0] =~ m/DOID_\d+/;
+  $vals[0] =~ m/DOID_(\d+)/;
   chop($vals[1]);
   my $g1 = substr($vals[1], rindex($vals[1], "/") + 1);
-  print $g1, "\t", $&, "\n";
+  print $g1, "\t", $1, "\n";
   if( defined($vals[2]) ){
     chop($vals[2]);
     my $g2 = substr($vals[2], rindex($vals[2], "/") + 1);
-    print $g2, "\t", $&, "\n";
+    print $g2, "\t", $1, "\n";
   }
 }
 
