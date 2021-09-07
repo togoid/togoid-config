9b58d434190048b2171f6d639ed0034e8a2a57df
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-09-07 16:48:44 +0900

    GlyTouCan IDとDOIDのペアを取得。

---
+++ config/glytoucan-doid/gen_pairs.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Fatal q/open/;
+use open ":utf8";
+
+while(<>){
+  chomp;
+  if (index($_, "<") != 0){
+    next;
+  }
+  my @vals = split /\t/;
+  $vals[0] =~ m/DOID_\d+/;
+  chop($vals[1]);
+  my $g1 = substr($vals[1], rindex($vals[1], "/") + 1);
+  print $g1, "\t", $&, "\n";
+  if( defined($vals[2]) ){
+    chop($vals[2]);
+    my $g2 = substr($vals[2], rindex($vals[2], "/") + 1);
+    print $g2, "\t", $&, "\n";
+  }
+}
+
+__END__
