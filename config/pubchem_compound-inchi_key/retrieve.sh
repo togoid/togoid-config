b25489995a75ccd29a70fec19a758c287a16615c
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-10-29 12:02:07 +0900

    並列度の見直しやコメントアウト業の削除。

--- config/pubchem_compound-inchi_key/retrieve.sh
+++ config/pubchem_compound-inchi_key/retrieve.sh
@@ -7,6 +7,7 @@ ENDPOINT=https://integbio.jp/rdf/pubchem/sparql
 WORKDIR=pubchem2inchikey # 一時的にIDペアファイルを保存するディレクトリ
 LIMIT=1000000 # SPARQLエンドポイントにおける取得可能データ件数の最大値
 CURL=/usr/bin/curl
+XARGS=/usr/bin/xargs
 
 # PubChem Compound IDとInChIKeyのペアを取得するクエリのテンプレート。
 # 100万件以上あるので、本スクリプト中で、OFFSET/LIMIT を sed で追加して用いる。
@@ -24,28 +25,33 @@ else
 fi
 
 C2P_TOTAL=$($CURL -sSH "Accept: text/csv" --data-urlencode query@$COUNT_PUBCHEM_INCHIKEY_QUERY_FILE $ENDPOINT | tail -1)
+#C2P_TOTAL=110040634
 #echo 取得対象C2P数: $C2P_TOTAL
 COUNT=$(expr $C2P_TOTAL / $LIMIT)
 
+declare -a query_list=()
 for i in $(seq 0 ${COUNT}); do
-  QUERY=$(sed -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" $PUBCHEM_INCHIKEY_QUERY_FILE)
-  $CURL -o ${WORKDIR}/${i}.txt -sSH "Accept: text/tab-separated-values" --data-urlencode query="$QUERY" $ENDPOINT
+  QUERY=$(sed -e "1 i #${i}" -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" $PUBCHEM_INCHIKEY_QUERY_FILE)
+  query_list+=( "${QUERY}" )
 done
 
+IFS=$'\t'
+echo -n "${query_list[*]}" | $XARGS -P 5 -i -d '\t' ./issue_query.sh ${WORKDIR} "{}" tsv $ENDPOINT 
+
 # エラー終了しているファイルを検索し、改めて検索、をエラーがなくなるまで行う。
-# ファイル冒頭に "chembl_id" が書かれていない場合に、エラーと判断する。
-ERROR_FILES=$(find ${WORKDIR} -type f -exec sh -c '(head -1 {} | grep -m 1 -q "^\"cid_str\"") || basename {} .txt' \;)
+# ファイル冒頭に "cid_str" が書かれていない場合に、エラーと判断する。
+ERROR_FILES=$(find ${WORKDIR} -type f -exec sh -c '(head -1 {} | grep -m 1 -q "^\"cid_str\"") || basename {} .tsv' \;)
 while true; do
   if [ -n "$ERROR_FILES" ]; then
      for i in $ERROR_FILES; do
        QUERY=$(sed -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" $PUBCHEM_INCHIKEY_QUERY_FILE)
-       $CURL -o ${WORKDIR}/${i}.txt -sSH "Accept: text/tab-separated-values" --data-urlencode query="$QUERY" $ENDPOINT
+       $CURL -o ${WORKDIR}/${i}.tsv -sSH "Accept: text/tab-separated-values" --data-urlencode query="$QUERY" $ENDPOINT
      done
-     ERROR_FILES=$(for f in $ERROR_FILES; do echo ${WORKDIR}/${f}.txt; done | xargs -i sh -c '(head -1 {} | grep -m 1 -q "^\"cid_str\"") || basename {} .txt')
+     ERROR_FILES=$(for f in $ERROR_FILES; do echo ${WORKDIR}/${f}.tsv; done | $XARGS -i sh -c '(head -1 {} | grep -m 1 -q "^\"cid_str\"") || basename {} .tsv')
      sleep 5
   else
      break
   fi
 done
 
-tail -qn +2 ${WORKDIR}/*.txt | sed -e 's/"//g'
+tail -qn +2 ${WORKDIR}/*.tsv | sed -e 's/"//g'
