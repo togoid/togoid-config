e3e5bb09ff9f35a3501fd5fa5ee5a9a4e2c537c6
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2022-02-15 13:55:52 +0900

    Translated comments to English

--- bin/select_offset_limit.sh
+++ bin/select_offset_limit.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
+#
+#  select_offset_limit.sh
+#    This script retrieves more than 1,000,000 records by making multiple requests with increasing offsets.
+#
+
 QUERY=$1
 ENDPOINT=$2
 
-# 結果を格納する一時ファイル作成
+# Create a temporary file to save the retrieved records
 TMPFILE=`mktemp /tmp/tmp-select_offset_limit.XXXXXX`
 
-# 変数初期化
+# Initialize variables
 LIMIT=100000
 offset=0
 num_results=0
-VERBOSE=0 # VERBOSE=1の時、LIMIT件ずつ時間を表示
+VERBOSE=0 # Show the cumulative number of the retrieved records in stderr at each request if VERBOSE=1
 
-# LIMIT件ずつ出力する
+# Retrieve $LIMIT records by each request
 while :
 do 
 
   if [ $VERBOSE -eq 1 ]; then echo $offset `date` >&2 ; fi
 
-  # SELECT実行する
+  # Request to the SPARQL endpoint
   cat $QUERY | echo $(cat) "OFFSET $offset LIMIT $LIMIT" | curl -s -H "Accept: text/csv" --data-urlencode "query@-" "$ENDPOINT" | sed -E 1d | perl -pe 's/,/\t/g; s/\"//g' >> $TMPFILE  
 
-  # 件数をカウントする
+  # Count the cumulative number of the retrieved records
   this_num_results=`wc -l $TMPFILE | awk '{print $1}'`
 
-  # 次のループを回すか判定する 
+  # Determine if the next query should be requested
   if [ $this_num_results -eq $num_results ] ; then break; fi
 
   offset=$(( $offset+$LIMIT )) 
@@ -32,7 +37,7 @@ do
 
 done
 
-# 結果を標準出力に出力する
+# Output the retrieved records to stdout
 cat $TMPFILE
 
 rm -f $TMPFILE
