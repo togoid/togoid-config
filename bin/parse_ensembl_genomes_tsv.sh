046256b60ac16a90c5a406b63196e58ba0dba67c
moriya-dbcls <moriya@dbcls.rois.ac.jp>
2022-03-15 22:47:42 +0900

    update script

--- bin/parse_ensembl_genomes_tsv.sh
+++ bin/parse_ensembl_genomes_tsv.sh
@@ -1,94 +1,93 @@
-#!/usr/bin/perl
+#!/bin/sh
+# Require: ls, gzip, head, tr, sed, awk ,sort ,uniq, echo
 
 # Ensembl Genome (Plants, Metazoa, Protists, Fungi, Bacteria) の TSV から ID 抽出
 # ensembl_xxx_gene (source) to other dataset (target)
 
-my $dir = $ARGV[0];      # e.g. ${TOGOID_ROOT}/ensembl_genomes/plants
 
-### dataset list
-# ensembl_transcript
-# ensembl_protein
-# ncbigene
-# renfeq_rna
-# refseq_protein
-# uniprot
-my $dataset = $ARGV[1];
+DIR=$1        # file dir. e.g. ${TOGOID_ROOT}/ensembl_genomes/plants
+DATASET=$2    # dataset list:
+              #     ensembl_transcript
+              #     ensembl_protein
+              #     ncbigene
+              #     renfeq_rna
+              #     refseq_protein
+              #     uniprot
 
-my $source = "gene_stable_id";
-my $db = "db_name";
-my $target;
-my $file_postfix;
-my $db_name;
-my $source_col;
-my $target_col;
-my $db_col;
-my $rm_target_version = 0;
+SRC="gene_stable_id"  # source colomn label
+DB="db_name"          # database colomn label
+DB_NAME=""            # database name for filtering
+RM_TRG_VER=0          # remove flag of target-ID-version-number
+CHK=""
 
-# check dataset
-if ($dataset eq "ensembl_transcript") {
-  $file_postfix = "ena";
-  $target = "transcript_stable_id";
-} elsif ($dataset eq "ensembl_protein") {
-  $file_postfix = "ena";
-  $target = "protein_stable_id";
-} elsif ($dataset eq "ncbigene") {
-  $file_postfix = "entrez";
-  $target = "xref";
-  $db_name = "EntrezGene";
-} elsif ($dataset eq "refseq_rna") {
-  $file_postfix = "refseq";
-  $target = "xref";
-  $db_name = "RefSeq_dna";
-  $rm_target_version = 1;
-} elsif ($dataset eq "refseq_protein") {
-  $file_postfix = "refseq";
-  $target = "xref";
-  $db_name = "RefSeq_peptide";
-  $rm_target_version = 1;
-} elsif ($dataset eq "uniprot") {
-  $file_postfix = "uniprot";
-  $target = "xref";
-  $db_name = "Uniprot";
-}
-
+# check target dataset
+if [ $DATASET = "ensembl_transcript" ] ; then
+  POSTFIX="ena"
+  TRG="transcript_stable_id"
+elif [ $DATASET = "ensembl_protein" ] ; then
+  POSTFIX="ena"
+  TRG="protein_stable_id"
+  CHK="transcript_stable_id"
+elif [ $DATASET = "ncbigene" ] ; then
+  POSTFIX="entrez"
+  TRG="xref"
+  DB_NAME="EntrezGene"
+elif [ $DATASET = "refseq_rna" ] ; then
+  POSTFIX="refseq"
+  TRG="xref"
+  DB_NAME="RefSeq_dna"
+  RM_TRG_VER=1
+elif [ $DATASET = "refseq_protein" ] ; then
+  POSTFIX="refseq"
+  TRG="xref"
+  DB_NAME="RefSeq_peptide"
+  RM_TRG_VER=1
+elif [ $DATASET = "uniprot" ] ; then
+  POSTFIX="uniprot"
+  TRG="xref"
+  DB_NAME="Uniprot"
+fi
+    
 # get file list
-my @files = `ls ${dir}/*.${file_postfix}.tsv.gz`;
+FILES=($(ls ${DIR}/*.${POSTFIX}.tsv.gz))
 
-# check column from a file
-chomp($files[0]);
-my @header = split(/\t/, `gzip -dc ${files[0]} | head -n 1`);
-for (my $i = 0; $i <= $#header; $i++) {
-  if ($header[$i] eq $source) {
-    $source_col = $i + 1;
-  }
-  if ($header[$i] eq $target) {
-    $target_col = $i + 1;
-  }
-  if ($header[$i] eq $db) {
-    $db_col = $i + 1;
-  }
-}
-
+# get source/target column from 1st file
+I=0
+HEADER=($(gzip -dc ${FILES[0]} | head -n 1 | tr "\t" "\n"))
+for COL in ${HEADER[@]}
+do
+  I=$(expr $I + 1)
+  if [ $COL = $SRC ] ; then
+    SRC_COL=$I
+  fi
+  if [ $COL = $TRG ] ; then
+    TRG_COL=$I
+  fi
+  if [ $COL = $DB ] ; then
+    DB_COL=$I
+  fi
+  if [ $COL = $CHK ] ; then
+    CHK_COL=$I
+  fi
+done
+    
 # output
-for (my $i = 0; $i <= $#files; $i++) {
-  chomp($files[$i]);
-  
-  # エスケープがややこしいので、場合分けで書き下し
-  if ($db_name) {
-    if ($rm_target_version) {
-      # check DB name & remove version number
-      print `gzip -dc ${files[$i]} | sed 1d | awk '{ if (\$${target_col} && \$${db_col} ~ "${db_name}") { printf "\%s\t\%s\\n", \$${source_col}, \$${target_col} } }' | sed -r 's/\\.[0-9]+\$//g' | sort | uniq`;
-    } else {
-      # check DB name
-      print `gzip -dc ${files[$i]} | sed 1d | awk '{ if (\$${target_col} && \$${db_col} ~ "${db_name}") { printf "\%s\t\%s\\n", \$${source_col}, \$${target_col} } }'                             | sort | uniq`;
-    }
-  } else {
-    if ($rm_target_version) {
-      # remove version number
-      print `gzip -dc ${files[$i]} | sed 1d | awk '{ if (\$${target_col})                               { printf "\%s\t\%s\\n", \$${source_col}, \$${target_col} } }' | sed -r 's/\\.[0-9]+\$//g' | sort | uniq`;
-    } else {
-      # normal
-      print `gzip -dc ${files[$i]} | sed 1d | awk '{ if (\$${target_col})                               { printf "\%s\t\%s\\n", \$${source_col}, \$${target_col} } }'                             | sort | uniq`;
-    }  
-  }
-}
+for FILE in ${FILES[@]}
+  do
+  if [ $DB_NAME ] && [ $RM_TRG_VER -eq 1 ] ; then
+    # check DB name & remove version number (refseq_rna, refseq_protein)
+    gzip -dc ${FILE} | sed 1d | awk -v src=$(echo ${SRC_COL}) -v trg=$(echo ${TRG_COL}) -v db=$(echo ${DB_COL}) -v name=${DB_NAME} '{ if ($trg && $db ~ name) { printf "%s\t%s\n", $src, $trg } }' | sed -r 's/\.[0-9]+$//g' | sort | uniq
+  elif [ $DB_NAME ] ; then
+    # check DB name (ncbigene, uniprot)
+    gzip -dc ${FILE} | sed 1d | awk -v src=$(echo ${SRC_COL}) -v trg=$(echo ${TRG_COL}) -v db=$(echo ${DB_COL}) -v name=${DB_NAME} '{ if ($trg && $db ~ name) { printf "%s\t%s\n", $src, $trg } }' | sort | uniq
+  elif [ $RM_TRG_VER -eq 1 ] ; then
+    # remove version number (-)
+    gzip -dc ${FILE} | sed 1d | awk -v src=$(echo ${SRC_COL}) -v trg=$(echo ${TRG_COL}) '{ if ($trg) { printf "%s\t%s\n", $src, $trg } }' | sed -r 's/\.[0-9]+$//g' | sort | uniq
+  elif [ $CHK ] ; then
+    # compare to transcript_id  (ensembl_protein)
+    gzip -dc ${FILE} | sed 1d | awk -v src=$(echo ${SRC_COL}) -v trg=$(echo ${TRG_COL}) -v chk=$(echo ${CHK_COL}) '{ if ($trg && $src != $trg && $trg != $chk) { printf "%s\t%s\n", $src, $trg } }' | sort | uniq
+  else
+    # normal (ensembl_transcript)
+    gzip -dc ${FILE} | sed 1d | awk -v src=$(echo ${SRC_COL}) -v trg=$(echo ${TRG_COL}) '{ if ($trg && $src != $trg) { printf "%s\t%s\n", $src, $trg } }' | sort | uniq
+  fi
+done
