e9eff129b7455078d4c87db4526d1ee11ddf624d
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2023-05-17 17:37:17 +0900

    add message to report pair count

--- config/pdb-go/retrieve.sh
+++ config/pdb-go/retrieve.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/sh
 set -euo pipefail
 
-# Chembl IDとInChI Keyのペアを取得する。
-
 ENDPOINT=https://integbio.jp/rdf/pdb/sparql
 LIMIT=1000000 # SPARQLエンドポイントにおける取得可能データ件数の最大値
 CURL=/usr/bin/curl
@@ -16,9 +14,9 @@ COUNT_QUERY_FILE=query_count.rq
 if [ ! -e $QUERY_FILE ]; then echo "必要なファイルが不足しています。:$QUERY_FILE"; exit; fi
 if [ ! -e $COUNT_QUERY_FILE ]; then echo "必要なファイルが不足しています。:$COUNT_QUERY_FILE"; exit; fi
 
-C2P_TOTAL=$($CURL -sSH "Accept: text/csv" --data-urlencode query@$COUNT_QUERY_FILE $ENDPOINT | tail -1)
-#echo 取得対象C2P数: $C2P_TOTAL
-COUNT=$(expr $C2P_TOTAL / $LIMIT)
+PAIR_TOTAL=$($CURL -sSH "Accept: text/csv" --data-urlencode query@$COUNT_QUERY_FILE $ENDPOINT | tail -1)
+echo "Number of pairs to fetch: $PAIR_TOTAL" 1>&2
+COUNT=$(expr $PAIR_TOTAL / $LIMIT)
 
 for i in $(seq 0 ${COUNT}); do
   QUERY=$(sed -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" $QUERY_FILE)
