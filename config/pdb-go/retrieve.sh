477c56d3ccf507d93d5a6ce87632161216b8a015
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2023-05-23 17:05:54 +0900

    split query by PDB IDs' first letter

--- config/pdb-go/retrieve.sh
+++ config/pdb-go/retrieve.sh
@@ -2,23 +2,18 @@
 set -euo pipefail
 
 ENDPOINT=https://integbio.jp/rdf/pdb/sparql
-LIMIT=1000000 # SPARQLエンドポイントにおける取得可能データ件数の最大値
 CURL=/usr/bin/curl
 
 # ペアを取得するクエリのテンプレート。
-# 100万件以上あるので、本スクリプト中で、OFFSET/LIMIT を sed で追加して用いる。
+# 100万件以上あるので、本スクリプト中で、PDB ID の先頭文字(1~9)ごとに取得するよう FILTER 部分を sed で編集して用いる。
 QUERY_FILE=query.rq
-# 上記クエリで得られる結果の全件数を取得するクエリのテンプレート。
-COUNT_QUERY_FILE=query_count.rq
 
 if [ ! -e $QUERY_FILE ]; then echo "必要なファイルが不足しています。:$QUERY_FILE"; exit; fi
-if [ ! -e $COUNT_QUERY_FILE ]; then echo "必要なファイルが不足しています。:$COUNT_QUERY_FILE"; exit; fi
 
-PAIR_TOTAL=$($CURL -sSH "Accept: text/csv" --data-urlencode query@$COUNT_QUERY_FILE $ENDPOINT | tail -1)
-echo "Number of pairs to fetch: $PAIR_TOTAL" 1>&2
-COUNT=$(expr $PAIR_TOTAL / $LIMIT)
-
-for i in $(seq 0 ${COUNT}); do
-  QUERY=$(sed -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" $QUERY_FILE)
+for i in $(seq 1 9); do
+  #TIME=`date "+%Y-%m-%d %H:%M:%S"`
+  #echo "[$TIME] Fetching " $i 1>&2
+  QUERY=$(sed -e "s/__NUM__/$i/g" $QUERY_FILE)
   $CURL -sSH "Accept: text/tab-separated-values" --data-urlencode query="$QUERY" $ENDPOINT | tail -qn +2 | sed -e 's/"//g'
+  #echo "$QUERY"
 done
