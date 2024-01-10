8394e6f211b3852e7668abd42869275fc555775b
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2024-01-10 12:12:00 +0900

    enable to fetch more than 1 million lines

--- bin/sparql_taxon.rb
+++ bin/sparql_taxon.rb
@@ -5,13 +5,18 @@ endpoint      = ARGV.shift || "https://rdfportal.org/ebi/sparql"
 taxonomy_list = ARGV.shift || "../../input/ensembl/taxonomy.txt"
 
 query_template = File.read(query_file)
+limit = 1000000
 
 File.read(taxonomy_list).split("\n").each do |taxon|
-  query = query_template.sub('{{taxon}}', taxon)
-  cmd = "curl -s -H 'Accept: text/csv' --data-urlencode 'query=#{query}' #{endpoint} | sed -E 1d"
-  IO.popen(cmd) do |io|
-    while buffer = io.gets
-      puts buffer.gsub(',', "\t").gsub('"', '')
+  i = 0
+  while i % limit == 0
+    query = query_template.sub('{{taxon}}', taxon) + " OFFSET #{i} LIMIT #{limit}"
+    cmd = "curl -s -H 'Accept: text/csv' --data-urlencode 'query=#{query}' #{endpoint} | sed -E 1d"
+    IO.popen(cmd) do |io|
+      while buffer = io.gets
+        puts buffer.gsub(',', "\t").gsub('"', '')
+        i += 1
+      end
     end
   end
 end
