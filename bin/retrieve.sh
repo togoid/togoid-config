0160a4d2345f444d082d8e5aadf6cb6367c10a0d
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2024-02-27 16:28:58 +0900

    add usage

--- bin/retrieve.sh
+++ bin/retrieve.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+function usage {
+    echo "Usage: $0 [OPTIONS] ENDPOINT
+
+  To get all results of a SPARQL query whose result reaches the limit set by the endpoint, repeatedly execute the query with OFFSET / LIMIT.
+  Firstly, query_count.rq in the working directory or the query specified by -c option is executed.
+  This is supposed to count the total number of result that query.rq retrieves.
+  The number of the repetition is determined according to the result and the limit number for each retrieval (1000000 or number specified by -l).
+  Then query.rq in the working directory or the query specified by -q option is repeatedly executed with OFFSET / LIMIT.
+  The results for each retrieval are output to temp/ directory.
+  If results are considered to be failed, the query is retried. If a query is failed for 3 consective times, exit with error.
+  Finally, all results are output to stdout.
+
+Argument:
+  ENDPOINT   : URI of SPARQL endpoint.
+
+Options:
+  -l NUM     : Number of result lines to be retrieved for each query execution. (default: 1000000)
+  -c FILE    : Filepath of SPARQL query to count total number of the result. (default: query_count.rq)
+  -q FILE    : Filepath of SPARQL query to be executed repeatedly. (default: query.rq)" 1>&2
+    exit 1
+}
+
 CURL=/usr/bin/curl
 
 ENDPOINT=""
@@ -23,6 +45,9 @@ do
         -c)
             QUERY_COUNT_FILE=$2
             shift
+            ;;
+        -h)
+            usage
             ;;
         *)
             ENDPOINT=$1
